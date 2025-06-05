0efc52d5a86ceacde4a8b96d08d48bd3cca823a9
Mallard-rc <teti250801teti@gmail.com>
2025-06-05 16:28:24 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,15 @@ API_KEY = "sk_cd7225a5b96a922efa4da311b752fdf96e70d009dca6a46d"
 client = ElevenLabs(api_key=API_KEY)
 
 def say(text, voice_name="Bella"):
-    voice = client.get_voices_by_name(voice_name)[0]
+    voices = client.get_voices()
+    voice = None
+    for v in voices:
+        if v.name == voice_name:
+            voice = v
+            break
+    if not voice:
+        print(f"Голос с именем '{voice_name}' не найден, использую первый доступный.")
+        voice = voices[0]
 
     audio_gen = client.text_to_speech.convert(text=text, voice=voice)
     audio_bytes = b"".join(chunk for chunk in audio_gen)
