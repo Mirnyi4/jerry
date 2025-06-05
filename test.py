7f429e5c948ac062398ffb69f8fbce1272344f26
Mallard-rc <teti250801teti@gmail.com>
2025-06-05 17:04:56 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
 from elevenlabs.client import ElevenLabs
 from elevenlabs import play
 
-# Подключаемся к ElevenLabs с ключом
+# Подключаемся к ElevenLabs
 client = ElevenLabs(
     api_key="sk_cd7225a5b96a922efa4da311b752fdf96e70d009dca6a46d"
 )
 
-# Генерация речи
-audio = client.generate(
-    text="Привет, это Джерри. Я снова в деле!",
+# Получаем поток речи (stream)
+stream = client.text_to_speech(
+    text="Привет, это Джерри. Говорю быстро и чётко!",
     voice="Dmitry",
-    model="eleven_multilingual_v2"
+    model="eleven_multilingual_v2",
+    stream=True
 )
 
-# Воспроизведение
-play(audio)
+# Проигрываем аудио
+play(stream)
