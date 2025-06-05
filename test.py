b185f84d23be269cf77520582404c09631cf5439
Mallard-rc <teti250801teti@gmail.com>
2025-06-05 17:06:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 from elevenlabs.client import ElevenLabs
 from elevenlabs import play
 
-# Подключаемся к ElevenLabs
+# Укажи свой API-ключ
 client = ElevenLabs(
     api_key="sk_cd7225a5b96a922efa4da311b752fdf96e70d009dca6a46d"
 )
 
-# Получаем поток речи (stream)
-stream = client.text_to_speech(
-    text="Привет, это Джерри. Говорю быстро и чётко!",
-    voice="Dmitry",
+# Получаем голосовой поток через метод .generate()
+stream = client.generate(
+    text="Привет, это Джерри. Я говорю быстро и чётко.",
+    voice="Dmitry",  # Можно указать другой голос, если хочешь
     model="eleven_multilingual_v2",
     stream=True
 )
 
-# Проигрываем аудио
+# Воспроизвести голос
 play(stream)
