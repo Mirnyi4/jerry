7aa948e420a3d25deabcfbd628ed6b088e8483f9
Mallard-rc <teti250801teti@gmail.com>
2025-06-13 13:10:56 +0300

    Create telegram_client.py

---
+++ telegram_client.py
@@ -0,0 +1,16 @@
+from telethon import TelegramClient
+
+# Твои данные
+api_id = 16131453
+api_hash = '700e8b63eaa842f1e5ab3c5a811dd3c8'
+
+# Создаём клиент — название сессии можешь поменять
+client = TelegramClient('session_jerry', api_id, api_hash)
+
+async def main():
+    me = await client.get_me()
+    print(f'Успешно подключено как: {me.first_name} (ID: {me.id})')
+
+# Запуск клиента
+with client:
+    client.loop.run_until_complete(main())
